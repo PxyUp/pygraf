d7cf5c8eca750860a3acb6664503242131f5c129
PxyUp <tduble94@gmail.com>
2015-10-20 19:49:03 +0300

    Change test.py

--- test.py
+++ test.py
@@ -11,6 +11,7 @@ import pygraf
 #z=pygraf.graf(8,[[2,1,2],[2,3,1],[3,1,2],[1,3,1],[1,2,5],[4,5,2],[5,4,1],[7,6,6],[6,7,8],[8,4,2]])#неоринетированный
 graph=pygraf.graf(5,[[2,1],[2,3],[3,1],[1,3],[1,2],[3,2],[4,5],[5,4]])
 print(graph.comp(False))
+print(graph.check_orient())
 #print(graph.print_dfs())#print [False,True,True]
 #print(z.check_orient())#проверяем ориентированность графа  #false - ориентирован #true-неоринтирован
 #z.dfs(1,0)
